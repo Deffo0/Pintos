4c2d6e839537a2326a8f98199f5c6d243bfc98d8
Deffo0 <73228199+Deffo0@users.noreply.github.com>
2022-10-15 08:42:44 +0200

    Phase_1 (Threads Phase)

---
+++ Phase_1/src/threads/fixed-point-math.c
@@ -0,0 +1,58 @@
+#include <threads/fixed-point-math.h>
+//
+// Created by louay on 5/11/22.
+//
+int makeFP_thenAdd(int x, int y);
+int makeFirstFP_thenADD(int x, int y);
+int addTwoFP(int x, int y);
+int makeFP_thenMultiply(int x, int y);
+int makeFirstFP_thenMultiply(int x, int y);
+int multiplyTwoFP(int x, int y);
+int makeFP_thenDivide(int x, int y);
+int makeFirstFP_thenDivide(int x, int y);
+int makeSecondFP_thenDivide(int x, int y);
+int divideTwoFP(int x, int y);
+int convertToFP(int x);
+int convertTOInt(int x);
+
+
+int makeFP_thenAdd(int x, int y){
+    return (x + y) * (1 << 14);
+}
+int makeFirstFP_thenADD(int x, int y){
+    return x * (1 << 14) + y;
+}
+int addTwoFP(int x, int y){
+    return x + y;
+}
+int makeFP_thenMultiply(int x, int y){
+    return (x * y) * (1 << 14);
+}
+int makeFirstFP_thenMultiply(int x, int y){
+    return x * y;
+}
+int multiplyTwoFP(int x, int y){
+    return ((int64_t)x) * y/ (1 << 14);
+}
+int makeFP_thenDivide(int x, int y){
+    return (x * (1 << 14) / y) ;
+}
+int makeFirstFP_thenDivide(int x, int y){
+    return x / y;
+}
+int makeSecondFP_thenDivide(int x, int y){
+    return x / y;
+}
+int divideTwoFP(int x, int y){
+    return ((int64_t)x * (1 << 14) / y);
+}
+int convertToFP(int x){
+    return x * (1 << 14);
+}
+int convertTOInt(int x){
+    if(x < 0) return (x - (1<<14) / 2) / (1 << 14);
+    return  (x + (1<<14) / 2) / (1 << 14);
+}
+
+
+
---
+++ Phase_1/src/threads/nice.h
@@ -0,0 +1,12 @@
+//
+// Created by louay on 5/12/22.
+//
+struct nice_value_real{
+    int niceValue;
+    //// the more nice the thread, the more easier it yields the cpu!
+};
+
+#ifndef PINTOSPROJECT_NICE_H
+#define PINTOSPROJECT_NICE_H
+
+#endif //PINTOSPROJECT_NICE_H
